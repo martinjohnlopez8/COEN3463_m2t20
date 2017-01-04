50ee7ed6c8248650126e0f5faf841a83ec094023
Tinjo <Tinjo@Martins-MacBook-Pro.local>
2017-01-04 22:07:13 +0800

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'MNM Caterers',
+  	slidersInfo: [
+	  		{
+			"backgroundImage":"",
+			"header":"Lorem Ipsum et Dolor",
+			"link":"",
+			"linkMessage":""
+	  		},
+	  		{
+			"backgroundImage":"",
+			"header":"Lorem Ipsum et Dolor",
+			"link":"",
+			"linkMessage":""
+	  		},
+	  		{
+			"backgroundImage":"",
+			"header":"Lorem Ipsum et Dolor",
+			"link":"",
+			"linkMessage":""
+	  		}
+		],
+  	 });
 });
 
 module.exports = router;
