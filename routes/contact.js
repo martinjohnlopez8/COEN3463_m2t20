dac748cc29cc8ffea3f8b1c15c6e966a84570c27
Tinjo <Tinjo@Martins-MacBook-Pro.local>
2017-01-05 04:13:27 +0800

    update contacts.jade and contacts.js

--- routes/contact.js
+++ routes/contact.js
@@ -4,7 +4,7 @@ var nodemailer = require('nodemailer');
 
 
 router.get('/', function(req, res, next) {
-  res.render('contact');
+  res.render('contact',{title: 'Contact Us'});
 });
 
 router.post('/', function(req, res, next) {
@@ -17,21 +17,22 @@ router.post('/', function(req, res, next) {
 	});
 
 	var mailOptions = {
-		from : 'MNM Catering <mnmtestmail@gmail.com>',
+		from : req.body.name +' <'+req.body.email+'>',
 		to : 'mnmtestmail@gmail.com',
-		subject : 'Website Message Submission',
-		text : 'Received new inquiry with the following details. Name: ' +req.body.name+ ' Email: ' +req.body.email+ ' Event: ' +req.body.event+ ' Message: ' +req.body.message,
-		html : '<p>Received new inquiry with the following details. </p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'<li>Event: '+req.body.event+'</li></li><li>Message: '+req.body.message+'</li></ul>'
+		subject : 'Inquiry',
+		text : 'Received new inquiry with the following details. Name: ' +req.body.name+ ' Email: ' +req.body.email+ ' Message: ' +req.body.message,
+		html : '<p>Received new inquiry with the following details. </p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
 	};
 
 	transporter.sendMail(mailOptions, function(error, info) {
 		if(error){
 			console.log(error);
-			res.redirect('/contact', {title: 'Contact Us', sent: 'no'});
+			res.redirect('/contact');
 		} else {
 			console.log('Message Sent: '+info.response);
-			res.redirect('/contact', {title: 'Contact Us', sent: 'yes'});
+			res.redirect('/contact');
 		}
 	});
 });
+
 module.exports = router;
